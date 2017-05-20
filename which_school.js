940026fea5aa2f10af49b6596a5cba0e08d365d9
Shauna Griffin <shaunahennessygriffin@gmail.com>
2017-05-20 12:05:32 +0000

    added which_school.js file

---
+++ which_school.js
@@ -0,0 +1,10 @@
+function whichSchool(age){
+if (age < 13)
+  return "Elementary School";
+else if (age >= 13 && age <= 18)
+  return "Secondary School";
+else if (age > 18)
+  return "Lighthouse Labs";
+}
+
+console.log(whichSchool(18));
